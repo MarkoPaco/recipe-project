1583b0cba68d01b59bd775a0d8c44e251dbcf7c8
markopaco <marko_stankovic@web.de>
2018-10-30 13:53:29 +0100

    Add view, controller and service for update ingredients.
    Signed-off-by: markopaco <marko_stankovic@web.de>

--- src/main/java/guru/springframework/recipeproject/services/IngredientService.java
+++ src/main/java/guru/springframework/recipeproject/services/IngredientService.java
@@ -5,4 +5,6 @@ import guru.springframework.recipeproject.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long inredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
 }
--- src/main/java/guru/springframework/recipeproject/services/IngredientServiceImpl.java
+++ src/main/java/guru/springframework/recipeproject/services/IngredientServiceImpl.java
@@ -1,9 +1,12 @@
 package guru.springframework.recipeproject.services;
 
 import guru.springframework.recipeproject.commands.IngredientCommand;
+import guru.springframework.recipeproject.converters.IngredientCommandToIngredient;
 import guru.springframework.recipeproject.converters.IngredientToIngredientCommand;
+import guru.springframework.recipeproject.domain.Ingredient;
 import guru.springframework.recipeproject.domain.Recipe;
 import guru.springframework.recipeproject.repositories.RecipeRepository;
+import guru.springframework.recipeproject.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +16,16 @@ import java.util.Optional;
 @Service
 public class IngredientServiceImpl implements IngredientService {
 
+    private final IngredientCommandToIngredient ingredientCommandToIngredient;
     private final IngredientToIngredientCommand ingredientToIngredientCommand;
     private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+    public IngredientServiceImpl(IngredientCommandToIngredient ingredientCommandToIngredient, IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.ingredientCommandToIngredient = ingredientCommandToIngredient;
         this.ingredientToIngredientCommand = ingredientToIngredientCommand;
         this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
     @Override
@@ -41,5 +48,41 @@ public class IngredientServiceImpl implements IngredientService {
         }
 
         return ingredientCommandOptional.get();
+    }
+
+    @Override
+    public IngredientCommand saveIngredientCommand(IngredientCommand command) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(command.getRecipeId());
+
+        if (!recipeOptional.isPresent()) {
+            log.error("Recipe nor found for id: " + command.getRecipeId());
+            return new IngredientCommand();
+        } else {
+            Recipe recipe = recipeOptional.get();
+
+            Optional<Ingredient> ingredientOptional = recipe
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.equals(command.getId()))
+                    .findFirst();
+
+            if (ingredientOptional.isPresent()) {
+                Ingredient ingredientFound = ingredientOptional.get();
+                ingredientFound.setAmount(command.getAmount());
+                ingredientFound.setDescription(command.getDescription());
+                ingredientFound.setUnitOfMeasure(unitOfMeasureRepository
+                        .findById(command.getUnitOfMeasure().getId())
+                        .orElseThrow(() -> new RuntimeException("Uom not found!")));
+            } else {
+                recipe.addIngredient(ingredientCommandToIngredient.convert(command));
+            }
+
+            Recipe savedRecipe = recipeRepository.save(recipe);
+
+            return ingredientToIngredientCommand.convert(savedRecipe.getIngredients().stream()
+                    .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
+                    .findFirst()
+                    .get());
+        }
     }
 }
